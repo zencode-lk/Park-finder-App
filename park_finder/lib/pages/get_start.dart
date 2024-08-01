26887a3c63e64061bf050691092a4f45aae564df
Manindu Ramawickrama <maninduramawickrama110@gmail.com>
2024-08-01 12:27:42 +0530

    login_page

---
+++ park_finder/lib/pages/get_start.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Park Finder',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GetStartedScreen(),
+    );
+  }
+}
+
+class GetStartedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              child: Text('Get Started'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              child: Text('I already have an account'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
