2f4912ae14bd8517ed4c842834e7a2775a92a5a1
Manindu Ramawickrama <maninduramawickrama110@gmail.com>
2024-08-09 08:48:26 +0530

    update
    get start page
    new image folder created

--- park_finder/lib/pages/get_start.dart
+++ park_finder/lib/pages/get_start.dart
@@ -1,9 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:go_router/go_router.dart';
 import 'package:park_finder/pages/user_login.dart';
 import 'package:park_finder/pages/user_register.dart';
-//import 'login.dart' as logIn;
-//import 'user_rigistration.dart' as userRegistration;
 
 void main() {
   runApp(MyApp());
@@ -26,32 +26,132 @@ class GetStartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                //userRegistration.main();
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context)=> UserRegister(), //loginRoute
-                ));
-
-              },
-              child: Text('Get Started'),
+      body: Stack(
+        children: [
+          // Background gradient
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF0D133E), // Darker blue
+                    Color(0xFF9575CD),
+                  ],
+                  stops: [0.3, 1.0], // Adjust the gradient stops to drag down the darker color
+                ),
+              ),
+              child: Stack(
+                children: [
+                  // Semi-transparent version of the logo in the left middle
+                  Positioned(
+                    left: 20,
+                    top: MediaQuery.of(context).size.height / 2 - 100,
+                    child: Opacity(
+                      opacity: 0.3, // Adjust the opacity value as needed
+                      child: Image.asset(
+                        'images/logio.png',
+                        height: 200, // Adjust height as needed
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  // Wave design in the middle
+                  Center(
+                    child: ClipPath(
+                      clipper: WaveClipper(),
+                      child: Container(
+                        color: Colors.white.withOpacity(0.3),
+                        height: 200,
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            TextButton(
-              onPressed: () {
-                //logIn.main();
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context)=> SignInScreen(),
-                ));
-              },
-              child: Text('I already have an account'),
+          ),
+          // Top-right logo
+          Positioned(
+            top: 20,
+            right: 20,
+            child: Image.asset(
+              'images/logio.png',
+              height: 50,
             ),
-          ],
-        ),
+          ),
+          // Centered content
+          Positioned.fill(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'PARK FINDER',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => UserRegister(),
+                    ));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => SignInScreen(),
+                    ));
+                  },
+                  child: Text(
+                    'I already have an account',
+                    style: TextStyle(color: Colors.white70, fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, size.height - 30);
+    path.quadraticBezierTo(
+        size.width / 4, size.height, size.width / 2, size.height - 30);
+    path.quadraticBezierTo(
+        size.width * 3 / 4, size.height - 60, size.width, size.height - 30);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
