a9a1ffb3c3075d6c320da7c9ab216648cd8279e6
Manindu Ramawickrama <148376441+maniii17@users.noreply.github.com>
2024-09-19 20:47:52 +0530

    Revert "terms and conditions"
    This reverts commit 73a33f23f693b744966cdfb747520f8d97714d7b.

--- park_finder/lib/pages/premium_user_dashboard.dart
+++ park_finder/lib/pages/premium_user_dashboard.dart
@@ -20,24 +20,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple[200],
-        title: Text(
-          'Parking App',
-          style: TextStyle(color: Colors.white),
-        ),
+        title: Text('Parking App'),
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Center(
               child: Text(
                 'Rs.5700.00',
-                style: TextStyle(fontSize: 18, color: Colors.white),
+                style: TextStyle(fontSize: 18),
               ),
             ),
           ),
         ],
         leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.white),
+          icon: Icon(Icons.menu),
           onPressed: () {},
         ),
       ),
@@ -45,76 +41,63 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            SizedBox(height: 16),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                buildCardButton(context, 'Schedule', Icons.schedule),
-                buildCardButton(context, 'Find Parking', Icons.local_parking),
-                buildCardButton(context, 'Reviews', Icons.rate_review),
+                buildButton(context, 'Schedule', Icons.schedule),
+                buildButton(context, 'Find Parking', Icons.local_parking),
+                buildButton(context, 'Reviews', Icons.rate_review),
               ],
             ),
             SizedBox(height: 16),
             Text(
               'Recent Activity',
-              style: TextStyle(fontSize: 20, color: Colors.white),
+              style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 8),
             buildActivity('Payment Successful - rs. 5700.00'),
             buildActivity('Parking Scheduled - Silva\'s Car Park'),
             buildActivity('Parking Deducted - rs. 500.00'),
             SizedBox(height: 16),
-            Spacer(), // Push the button to the bottom
+            Spacer(), // To push the button to the bottom of the screen
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => VehicleRegistrationForm()));
+                  builder: (context) => VehicleRegistrationForm() 
+                ));
               },
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                backgroundColor: Colors.deepPurple[300],
               ),
               child: Text(
-                'Payment Method',
-                style: TextStyle(fontSize: 18, color: Colors.white),
+                'Register Vehicle',
+                style: TextStyle(fontSize: 18),
               ),
             ),
             SizedBox(height: 16),
           ],
         ),
       ),
-      backgroundColor: Colors.purple[100], // Light purple background
     );
   }
 
-  Widget buildCardButton(BuildContext context, String title, IconData icon) {
-    return GestureDetector(
-      onTap: () {
+  Widget buildButton(BuildContext context, String title, IconData icon) {
+    return ElevatedButton(
+      onPressed: () {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('$title button pressed')),
         );
       },
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        elevation: 5,
-        child: Container(
-          width: 100,
-          height: 100,
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(icon, size: 30, color: Colors.blue),
-              SizedBox(height: 5),
-              Text(
-                title,
-                style: TextStyle(fontSize: 16, color: Colors.black),
-              ),
-            ],
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, size: 30),
+          SizedBox(height: 5),
+          Text(
+            title,
+            style: TextStyle(fontSize: 16),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -129,7 +112,7 @@ class HomeScreen extends StatelessWidget {
           Expanded(
             child: Text(
               activity,
-              style: TextStyle(fontSize: 16, color: Colors.black),
+              style: TextStyle(fontSize: 16),
             ),
           ),
         ],
